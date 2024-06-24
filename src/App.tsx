0c3d2b85a29309077ef5b91277c9fe2b27c460d3
jair-chaguay <jchaguay@espol.edu.ec>
2024-06-24 14:21:22 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import BasicTable from './components/BasicTable';
-import './App.css'
+import './App.css';
+import WeatherChart from './components/WeatherChart';
+import ControlPanel from './components/ControlPane';
 
 
 function App() {
@@ -30,10 +32,17 @@ function App() {
       <Grid xs={6} sm={4} md={3} lg={2}>
         <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
         <Summary></Summary>
-      </Grid>
+      </Grid>      
       <Grid xs={12} md={6} lg={9}>
         <BasicTable />
       </Grid>
+      <Grid xs={12} lg={2}>
+             <ControlPanel/>
+        </Grid>
+      <Grid xs ={12} lg={10}>
+        <WeatherChart></WeatherChart>
+      </Grid>
+      
     </Grid>
 
   )
