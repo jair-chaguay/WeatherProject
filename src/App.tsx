5543fba6d00d74974e21bd71a5d0c8b83f94e770
Moises Alvarez <jchaguay@espol.edu.ec>
2024-06-26 14:50:02 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
@@ -9,18 +10,61 @@ import ControlPanel from './components/ControlPane';
 
 function App() {
 
+          en el arreglo de resultados
+      */}
+
+      let location = xml.getElementsByTagName("location")[1]
+
+      let geobaseid = location.getAttribute("geobaseid")
+      dataToIndicators.push(["Location", "geobaseid", geobaseid])
+
+      let latitude = location.getAttribute("latitude")
+      dataToIndicators.push(["Location", "Latitude", latitude])
+
+      let longitude = location.getAttribute("longitude")
+      dataToIndicators.push(["Location", "Longitude", longitude])
+
+
+      console.log(dataToIndicators)
+
+      {/*Renderice el arreglo de resultados en un arreglo de elementos Indicator*/ }
+      let indicatorsElements = Array.from(dataToIndicators).map(
+        (element) => <Indicator title={element[0]} subtitle={element[1]} value={element[2]} />
+      )
+
+      {/*Modificación de la variable de estado mediante a función de actualización*/ }
+
+      setIndicators(indicatorsElements)
+
+      let arrayObjects = Array.from(xml.getElementsByTagName("time")).map((timeElement) => {
+        let rangeHours = timeElement.getAttribute("from").split("T")[1] + " - " + timeElement.getAttribute("to").split("T")[1]
+        let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " " + timeElement.getElementsByTagName("windDirection")[0].getAttribute("code")
+        return { "rangeHours": rangeHours, "windDirection": windDirection }
+      })
+
+      arrayObjects = arrayObjects.slice(0, 8)
+      setRowsTable(arrayObjects)
+
+    })()
+  }, [])
+
+
+  {/* Función para el efecto secundario a ejecutar y Arreglo de dependencias */ }
+
+
+
   return (
     <Grid container spacing={5}>
-      <Grid xs={12} sm={4} md={3} lg={2}>
-        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      <Grid xs={6} lg={2}>
+        {indicators[0]}
       </Grid>
-      <Grid xs={6} sm={4} md={3} lg={2}>
-        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      <Grid xs={6} lg={2}>
+        {indicators[1]}
       </Grid>
-      <Grid xs={6} sm={4} md={3} lg={2}>
-        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      <Grid xs={6} lg={2}>
+        {indicators[2]}
       </Grid>
-      <Grid xs={12} sm={4} md={3} lg={2}>
+      <Grid xs={6} lg={2}>
         <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
       </Grid>
       <Grid xs={6} sm={4} md={6} lg={2}>
@@ -32,17 +76,17 @@ function App() {
       <Grid xs={6} sm={4} md={3} lg={2}>
         <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
         <Summary></Summary>
-      </Grid>      
-      <Grid xs={12} md={6} lg={9}>
-        <BasicTable />
+      </Grid>
+      <Grid xs={12} lg={8}>
+        <BasicTable rows={rowsTable}></BasicTable>
       </Grid>
       <Grid xs={12} lg={2}>
-             <ControlPanel/>
-        </Grid>
-      <Grid xs ={12} lg={10}>
+        <ControlPanel />
+      </Grid>
+      <Grid xs={12} lg={10}>
         <WeatherChart></WeatherChart>
       </Grid>
-      
+
     </Grid>
 
   )
--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,6 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+/*
 function createData(
   name: string,
   calories: number,
@@ -15,7 +17,8 @@ function createData(
 ) {
   return { name, calories, fat, carbs, protein };
 }
-
+*/
+/*
 const rows = [
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
@@ -23,9 +26,28 @@ const rows = [
   createData('Cupcake', 305, 3.7, 67, 4.3),
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
+*/
 
-export default function BasicTable() {
+interface Config {
+  rows: Array<object>;
+}
+
+export default function BasicTable( data:Config ) {
+
+  let [rows, setRows] = useState([])
+
+  useEffect( () => {
+
+    (()=> {
+
+        setRows(data.rows)
+
+    })()
+
+}, [data] )
+
   return (
+    
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
